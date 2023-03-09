15bffd51efdbf18a56183f87842298f25698bb30
Junior Paixao <juniortidesenvolvimento1@gmail.com>
2023-03-09 16:53:43 -0300

    minuto 17
    minutos 17

---
+++ js/index.js
@@ -0,0 +1,52 @@
+const items = [
+    {
+        id: 0,
+        nome: 'camiseta',
+        img: '/image/produto-teste.png',
+        quantidade: 0
+    },
+    {
+        id: 1,
+        nome: 'short',
+        img: '/image/produto-teste.png',
+        quantidade: 0
+    },
+    {
+        id: 2,
+        nome: 'sapato',
+        img: '/image/produto-teste.png',
+        quantidade: 0
+    }
+]
+
+inicializarLoja = () => {
+    var containerProdutos = document.getElementById('produtos');
+    items.map((val)=>{
+        containerProdutos.innerHTML += `
+
+            <div class="produto-single">
+                <img src="`+val.img+`" />
+                <p>`+val.nome+`</p>
+                <a key="`+val.id+`" href="#">Adicionar ao carrinho!</a>
+            </div>
+
+        `;
+    })
+}
+
+inicializarLoja();
+
+atualizarCarrinho = () => {
+    console.log(items);
+}
+
+var links = document.getElementsByTagName('a');
+
+for(var i = 0; i < links.length; i++){
+    links[i].addEventListener("click", function(){
+        let key = this.getAttribute('key');
+        items[key].quantidade++;
+        atualizarCarrinho();
+        return false;
+    })
+}
